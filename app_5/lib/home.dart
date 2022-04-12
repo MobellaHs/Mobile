b20d0eeb3571b0bc37d734e455c3b21a03616769
Isabella Puttini <isabellaputtini@gmailcom>
2022-04-12 15:10:24 -0300

    adicionando imagem

--- app_5/lib/home.dart
+++ app_5/lib/home.dart
@@ -1,57 +1,44 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Home(),
-  ));
-}
-
 class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
 
-  String _info = "Informe seus dados.";
+  String _infoText = "Informe seus dados";
 
-  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  void _calculate() {
+    setState(() {
+      double? weight = double.tryParse(weightController.text);
+      double? height = double.tryParse(heightController.text);
 
-  TextEditingController pesoController = TextEditingController();
-  TextEditingController alturaController = TextEditingController();
+      if (weight == null || height == null) {
+        _infoText =
+            "Preencha os campos com números válidos. (Adicionar . se necessário)";
+      } else {
+        height = height / 100;
 
-  void _resetFields()
-  {
-      pesoController.text = '';
-      alturaController.text = '';
-      setState(() {
-        _info = "Informe seus dados.";
-        _formKey = GlobalKey<FormState>();
-      });
-  }
-
-  void _calcular(){
-    setState(()
-    {
-      double peso = double.parse(pesoController.text);
-      double altura = double.parse(alturaController.text) / 100;
-      double imc = peso / ( altura * altura);
-      print(imc);
-      if(imc < 18.6){
-        _info = 'Abaixo do Peso (${imc.toStringAsPrecision(3)})';
-      } else if(imc >= 18.6 && imc < 24.9){
-        _info = 'Peso Ideal (${imc.toStringAsPrecision(3)})';
-      } else if(imc >= 24.9 && imc < 29.9){
-        _info = 'Levemente Acima do Peso (${imc.toStringAsPrecision(3)})';
-      } else if(imc >= 29.9 && imc < 34.9){
-        _info = 'Obesidade Grau I (${imc.toStringAsPrecision(3)})';
-      } else if(imc >= 34.9 && imc < 39.9){
-        _info = 'Obesidade Grau II (${imc.toStringAsPrecision(3)})';
-      } else if(imc >= 40){
-        _info = 'Obesidade Grau III (${imc.toStringAsPrecision(3)})';
+        double imc = weight / (height * height);
+        if (imc < 18.6) {
+          _infoText = "Abaixo do Peso (${imc.toStringAsPrecision(4)})";
+        } else if (imc >= 18.6 && imc < 24.9) {
+          _infoText = "Peso Ideal (${imc.toStringAsPrecision(4)})";
+        } else if (imc >= 24.9 && imc < 29.9) {
+          _infoText = "Levemente Acima do Peso (${imc.toStringAsPrecision(4)})";
+        } else if (imc >= 29.9 && imc < 34.9) {
+          _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(4)})";
+        } else if (imc >= 34.9 && imc < 39.9) {
+          _infoText = "Obesidade Grau II (${imc.toStringAsPrecision(4)})";
+        } else if (imc >= 40) {
+          _infoText = "Obesidade Grau III (${imc.toStringAsPrecision(4)})";
+        }
       }
-
     });
   }
 
@@ -59,75 +46,81 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Calcula IMC"),
-        centerTitle: true,
-        backgroundColor: Colors.pink,
-        actions: <Widget>[
-
-        ],
+        title: Text("Calcular IMC"),
+        backgroundColor: Colors.deepPurple,
       ),
-
-      backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-        child: Form(
-          key: _formKey,
+      body: Container(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(32),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Icon(Icons.person_outline, size: 120.0, color: Colors.pink),
-              TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    labelText: "Peso (kg)",
-                    labelStyle: TextStyle(color: Colors.pink)),
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.pink, fontSize: 25.0),
-                controller: pesoController,
-                validator: (value){
-                  if(value!.isEmpty){
-                    return "Insira seu Peso!";
-                  }
-                },
-              ),
-              TextFormField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    labelText: "Altura (CM)",
-                    labelStyle: TextStyle(color: Colors.pink)),
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.pink, fontSize: 25.0),
-                controller: alturaController,
-                validator: (value){
-                  if(value!.isEmpty){
-                    return "Insira sua Altura!";
-                  }
-                },
+              Center(
+                child: Image.network(
+                  'https://kubomusical.com/wp-content/uploads/2020/06/dua-lipa-2019-3.jpg'
+                  height: 200,
+                  width: 200,
+                ),
               ),
               Padding(
-                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-                child: Container(
-                  height: 50,
-                  child: RaisedButton(
-                    onPressed: (){
-                      if(_formKey.currentState!.validate()){
-                        _calcular();
-                      }
-                    },
-                    child: Text('Calcular',style: TextStyle(color: Colors.white, fontSize: 25.0),),
-                    color: Colors.pink,
+                padding: EdgeInsets.only(bottom: 10),
+                child: Text(
+                  "Calcule seu IMC",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
-              Text(_info,
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.pink, fontSize: 25),
-              
-              )
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Digite seu peso (kg). Ex: 65.5",
+                ),
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+                controller: weightController,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Digite sua altura (cm). Ex: 175",
+                ),
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+                controller: heightController,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: RaisedButton(
+                  color: Colors.green,
+                  textColor: Colors.white,
+                  padding: EdgeInsets.all(15),
+                  child: Text(
+                    "Verificar",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  onPressed: _calculate,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Text(
+                  _infoText,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
             ],
           ),
-        )
-      )
+        ),
+      ),
     );
   }
 }

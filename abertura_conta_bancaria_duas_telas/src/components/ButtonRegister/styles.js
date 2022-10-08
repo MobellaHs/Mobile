e7763d2a8399ade3295cc213485ca7e06c2b1038
Isabella Puttini <RM87624@fiap.com>
2022-10-07 22:41:27 -0300

    consulta de cep

---
+++ abertura_conta_bancaria_duas_telas/src/components/ButtonRegister/styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  button: {
+    flex: 1,
+    height: 50,
+    marginHorizontal: 15,
+    marginBottom: 15,
+    borderRadius: 5
+  },
+  buttonBlue: {
+    backgroundColor: '#821E7A'
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: '#FFF',
+    lineHeight: 50
+  }
+})
+
+export { styles }
